692b8891f908ba1fa07bc858d68e420c5d039828
Ochiyeng <deanwellsg@gmail.com>
2022-07-12 20:25:16 +0300

    Initial Commit

---
+++ src/main/java/com/botapplication/usersbotapplication/UsersBotApplication.java
@@ -0,0 +1,13 @@
+package com.botapplication.usersbotapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/botapplication/usersbotapplication/UsersBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.botapplication.usersbotapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
